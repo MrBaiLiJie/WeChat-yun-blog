aa87d9c28efbd5187fb53e49cad1274d7292b858
bailijie <473669799@qq.com>
2020-03-13 18:06:36 +0800

    后台框架已搭建完成

---
+++ src/views/form/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="app-container">
+    <el-form ref="form" :model="form" label-width="120px">
+      <el-form-item label="博客标题">
+        <el-input v-model="form.name" />
+      </el-form-item>
+      <el-form-item label="类型">
+        <el-select v-model="form.region" placeholder="请选择文章类型">
+          <el-option label="原创" />
+          <el-option label="转载" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="发布时间">
+        <el-col :span="11">
+          <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%;" />
+        </el-col>
+        <el-col :span="2" class="line">-</el-col>
+        <el-col :span="11">
+          <el-time-picker v-model="form.date2" type="fixed-time" placeholder="Pick a time" style="width: 100%;" />
+        </el-col>
+      </el-form-item>
+      <el-form-item label="Instant delivery">
+        <el-switch v-model="form.delivery" />
+      </el-form-item>
+      <el-form-item label="Article type">
+        <el-checkbox-group v-model="form.type">
+          <el-checkbox label="Vue" name="type" />
+          <el-checkbox label="Uni-app" name="type" />
+          <el-checkbox label="Js" name="type" />
+          <el-checkbox label="Apicloud" name="type" />
+        </el-checkbox-group>
+      </el-form-item>
+      <!-- <el-form-item label="Resources">
+        <el-radio-group v-model="form.resource">
+          <el-radio label="Sponsor" />
+          <el-radio label="Venue" />
+        </el-radio-group>
+      </el-form-item> -->
+      <el-form-item label="Article form">
+        <el-input v-model="form.desc" type="textarea" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Create</el-button>
+        <el-button @click="onCancel">Cancel</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$message('submit!')
+    },
+    onCancel() {
+      this.$message({
+        message: 'cancel!',
+        type: 'warning'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.line{
+  text-align: center;
+}
+</style>
+
---
+++ src/views/swiper/swiper.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="swiper-container">
+    <div class="swiper-text">轮播图管理</div>
+    <button>点击上传</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Swiper',
+  computed: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
